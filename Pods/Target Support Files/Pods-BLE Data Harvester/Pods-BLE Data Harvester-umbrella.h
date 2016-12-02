00581b7d8eea7f953590149948314e520e773213
Sam Presley <samp94@gmail.com>
2016-12-02 21:21:54 +0000

    added CocoaMQTT and Cocoa pods

---
+++ Pods/Target Support Files/Pods-BLE Data Harvester/Pods-BLE Data Harvester-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BLE_Data_HarvesterVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BLE_Data_HarvesterVersionString[];
+
---
+++ Pods/Target Support Files/Pods-BLE Data HarvesterUITests/Pods-BLE Data HarvesterUITests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_BLE_Data_HarvesterUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_BLE_Data_HarvesterUITestsVersionString[];
+
