6bf8942d6ea5de2438c8574c29386204e71051fb
Sam Presley <samp94@gmail.com>
2016-12-02 20:47:36 +0000

    initial commit blank tab view

---
+++ BLE Data Harvester/BLE Data Harvester/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BLE Data Harvester
+//
+//  Created by Sam Presley on 02/12/2016.
+//  Copyright © 2016 ELEC6245. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BLE Data Harvester/BLE Data HarvesterTests/BLE_Data_HarvesterTests.swift
@@ -0,0 +1,36 @@
+//
+//  BLE_Data_HarvesterTests.swift
+//  BLE Data HarvesterTests
+//
+//  Created by Sam Presley on 02/12/2016.
+//  Copyright © 2016 ELEC6245. All rights reserved.
+//
+
+import XCTest
+@testable import BLE_Data_Harvester
+
+class BLE_Data_HarvesterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
