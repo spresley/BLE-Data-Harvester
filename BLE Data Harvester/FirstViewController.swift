a8ed293d0e829bb7d9af1e6fd536e65b93d548c8
Nathan Ruttley <nathan@nathanr.co.uk>
2016-12-02 21:23:23 +0000

    corebluetooth

--- BLE Data Harvester/FirstViewController.swift
+++ BLE Data Harvester/FirstViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import CoreBluetooth
+
+var centralManager:CBCentralManager!
+var sensorTag:CBPeripheral?
 
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +28,24 @@ class FirstViewController: UIViewController {
 
 }
 
+func centralManagerDidUpdateState(central: CBCentralManager){
+    switch central.state {
+    case .PoweredOn:
+        // 1
+        keepScanning = true
+        // 2
+        _ = NSTimer(timeInterval: timerScanInterval, target: self, selector: #selector(pauseScan), userInfo: nil, repeats: false)
+        // 3
+        centralManager.scanForPeripheralsWithServices(nil, options: nil)
+    case .PoweredOff:
+        state = "Bluetooth on this device is currently powered off."
+    case .Unsupported:
+        state = "This device does not support Bluetooth Low Energy."
+    case .Unauthorized:
+        state = "This app is not authorized to use Bluetooth Low Energy."
+    case .Resetting:
+        state = "The BLE Manager is resetting; a state update is pending."
+    case .Unknown:
+        state = "The state of the BLE Manager is unknown."
+    }
+}
