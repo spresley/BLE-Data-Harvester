9b9e6ecfb0d25f3a53e98a6d9f3b482a6e806977
Sam Presley <samp94@gmail.com>
2016-12-04 00:01:33 +0000

    mqtt compiling and connecting

---
+++ BLE Data Harvester/MQTT-config.swift
@@ -0,0 +1,15 @@
+//
+//  MQTT-config.swift
+//  BLE Data Harvester
+//
+//  Created by Sam Presley on 02/12/2016.
+//  Copyright © 2016 ELEC6245. All rights reserved.
+//
+
+import Foundation
+
+public class MQTTlogin{
+    public static var host: String { return "f6z0bl.messaging.internetofthings.ibmcloud.com" }
+    public static var port: UInt16 { return 1883 }
+    public static var clientID: String { return "d:f6z0bl:iPhone:E1B33293-EA45-4A84-BF4F-ED1D64C91E86" }
+}
--- BLE Data Harvester/SecondViewController.swift
+++ BLE Data Harvester/SecondViewController.swift
@@ -7,19 +7,82 @@
 //
 
 import UIKit
+import CocoaMQTT
+
 
 class SecondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let clientID = "CocoaMQTT-" + String(ProcessInfo().processIdentifier)
+        print("Client ID = \(clientID)")
+        let mqtt = CocoaMQTT(clientID: MQTTlogin.clientID, host: MQTTlogin.host, port: MQTTlogin.port)
+        mqtt.username = "use-token-auth"
+        mqtt.password = "test1234"
+       // mqtt.willMessage = CocoaMQTTWill(topic: "/will", message: "dieout")
+        mqtt.keepAlive = 90
+        mqtt.delegate = self
+        mqtt.connect()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension SecondViewController: CocoaMQTTDelegate {
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
+        print("didConnect \(host):\(port)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        print("didConnectAck \(ack.rawValue)")
+        if ack == .accept {
+            print("connected OK")
+        }
+        
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        print("didPublishMessage with message: \(message.string)")
+        print("topic: \(message.topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        print("didPublishAck with id: \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        print("didReceivedMessage: \(message.string) with id \(id)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        print("didSubscribeTopic to \(topic)")
+    }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        print("didUnsubscribeTopic to \(topic)")
+    }
+    
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        print("didPing")
+    }
+    
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        _console(info:"didReceivePong")
+    }
+    
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        _console(info:"mqttDidDisconnect")
+    }
+    
+    func _console(info: String) {
+        print("Delegate: \(info)")
+    }
+    
+}
