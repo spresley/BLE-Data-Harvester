c9cc6707a9de60a9745235f2339a7b7af7fcf154
Sam Presley <samp94@gmail.com>
2016-12-26 14:25:11 +0000

    Tidy up of working json parse - need to create class

--- BLE Data Harvester/GraphViewController.swift
+++ BLE Data Harvester/GraphViewController.swift
@@ -28,9 +28,8 @@ class GraphViewController: UIViewController {
         let cloudantUsername = keysdict?["cloudant-username"] as! String
         let cloudantPassword = keysdict?["cloudant-password"] as! String
         
-       let url = URL(string: ("https://" + cloudantUsername + ":" + cloudantPassword + "@f810fc6b-39be-4c4c-9716-47f93c071d09-bluemix.cloudant.com/test-data/_design/design_doc/_view/by-date-minimised?limit=2"))!
+       let url = URL(string: ("https://" + cloudantUsername + ":" + cloudantPassword + "@f810fc6b-39be-4c4c-9716-47f93c071d09-bluemix.cloudant.com/test-data/_design/design_doc/_view/by-date-minimised"))!
         let task = URLSession.shared.dataTask(with: url){(data, response, error) in
-        
             if error != nil{
                 print("error")
             } else{
@@ -44,57 +43,24 @@ class GraphViewController: UIViewController {
                                 // access individual value in dictionary
                                 print(total_rows)
                             }
-                            
                             if let rows = dictionary["rows"]{
-//                                print(rows)
-//                                print("Row 0:")
-//                                print(rows[0])
-//                                print("Row 1:")
-//                                print(rows[1])
-                                
                                 for (row) in rows as! [AnyObject] {
-//                                    print(row)
                                     if let value = row["value"] as? [String: AnyObject]{
                                         print(value)
-//                                        print(value["activity_level"])
-                                        
+                                        if let time_stamp = value["time_stamp"] as? String {
+                                            print(time_stamp)
+                                            if let activity_level = value["activity_level"] as? Int {
+                                                print(activity_level)
+                                                if let light_level = value["light_level"] as? Int {
+                                                    print(light_level)
+                                                    //Add data to array here
+                                                }
+                                            }
+                                        }
                                     }
-                                
                                 }
                             }
-                            
-
-                            
-//                            if let firstObject = rows.first {
-//                                // access individual object in array
-//
-//                                
-//                            }
-
-
-//                            if let rows = dictionary["rows"] as? Int {
-//                                // access individual value in dictionary
-//                                print(rows)
-//                            }
-                            
-
-//                            for (key, value) in dictionary {
-//                                // access all key / value pairs in dictionary
-//                                print(key)
-//                                print(value)
-//                            }
-                            
-//                            if let nestedDictionary = dictionary["rows"] as? [String: Any] {
-//                                // access nested dictionary values by key
-//                                print(nestedDictionary)
-//                            }
-//
-//                            if let nestedDictionary = dictionary["anotherKey"] as? [String: Any] {
-//                                // access nested dictionary values by key
-//                            }
                         }
-
-                
                     }
                     catch{
                         print("Json processing failed")
