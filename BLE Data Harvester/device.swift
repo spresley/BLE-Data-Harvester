6ee71c5156ee6a2ed942ca9ccf7c15645a249205
Nathan Ruttley <nathan@nathanr.co.uk>
2017-01-09 17:03:20 +0000

    Trying background scanning

--- BLE Data Harvester/device.swift
+++ BLE Data Harvester/device.swift
@@ -13,7 +13,7 @@ struct Device {
     
     static let SensorTagAdvertisingUUID = "ROOM"
     
-    static let RoomMonitorServiceUUID =      "8C877E45-EE0A-4747-B9CB-4E17B11FC3EC" //"AC8BA2AE-F020-4395-A301-6E833EBD4571"
+    static let RoomMonitorServiceUUID =             "8C877E45-EE0A-4747-B9CB-4E17B11FC3EC" //"AC8BA2AE-F020-4395-A301-6E833EBD4571"
     static let MostRecentLightLevelUUID =           "8C8724F4-EE0A-4747-B9CB-4E17B11FC3EC" //"BC8B3A9A-F020-4395-A301-6E833EBD4571"
     static let MostRecentActivityStateUUID =        "8C870296-EE0A-4747-B9CB-4E17B11FC3EC" //"BC8BA6D8-F020-4395-A301-6E833EBD4571"
     static let HistoricalLightLevelUUID =           "8C8794B9-EE0A-4747-B9CB-4E17B11FC3EC" // "AC8B3A9A-F020-4395-A301-6E833EBD4571"
